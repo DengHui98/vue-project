e5128a1c3027cc46163a326c3eb09f84e6f15a61
DengHui98 <rdh1776222611@163.com>
2024-06-07 23:56:32 +0800

    Avatar 组件提交

---
+++ vue2-blog/src/components/Avatar.vue
@@ -0,0 +1,29 @@
+<template>
+	<img
+		class="avatar"
+		:src="url"
+		:style="{ width: `${size}px`, height: `${size}px` }"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		url: {
+			type: String,
+			require: true,
+		},
+		size: {
+			type: Number,
+			default: 30,
+		},
+	},
+};
+</script>
+
+<style scoped>
+.avatar {
+	border-radius: 50%;
+	display: inline-block;
+}
+</style>
