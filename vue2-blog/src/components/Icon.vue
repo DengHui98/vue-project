c9589efc1339114fda9f6e025f6da01d19acb1e1
DengHui98 <rdh1776222611@163.com>
2024-06-11 22:35:04 +0800

    图标组件

---
+++ vue2-blog/src/components/Icon.vue
@@ -0,0 +1,34 @@
+<template>
+	<i
+		class="iconfont"
+		:class="iconClass"
+	></i>
+</template>
+
+<script>
+const classMap = {
+	home: "icon-zhuye",
+	qq: "icon-qq",
+	weixin: "icon-weixin",
+	chaxun: "icon-chaxun",
+	break: "icon-xiangzuozhuan",
+};
+export default {
+	props: {
+		type: {
+			type: String,
+			require: true,
+		},
+	},
+	computed: {
+		iconClass() {
+			console.log(classMap[this.type]);
+			return classMap[this.type];
+		},
+	},
+};
+</script>
+
+<style scoped>
+@import url(//at.alicdn.com/t/c/font_4581694_vwscqeybscq.css);
+</style>
