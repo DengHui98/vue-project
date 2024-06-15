b9d5692d9be8bffbc3826070d990243b25e8b49a
DengHui98 <rdh1776222611@163.com>
2024-06-15 22:29:39 +0800

    layout 组件

---
+++ vue2-blog/src/components/Layout/test.vue
@@ -0,0 +1,47 @@
+<template>
+	<div class="test-container">
+		<Layout>
+			<template v-slot:left>
+				<div class="left">left</div>
+			</template>
+			<template>
+				<div class="main">main</div>
+			</template>
+			<template v-slot:right>
+				<div class="right">right</div>
+			</template>
+		</Layout>
+	</div>
+</template>
+
+<script>
+import "@/styles/global.less";
+import Layout from "./";
+export default {
+	components: {
+		Layout,
+	},
+};
+</script>
+
+<style scoped lang="less">
+.test-container {
+	height: 100vh;
+	overflow: auto;
+	.left {
+		width: 100px;
+		background: red;
+		height: 100%;
+	}
+	.main {
+		width: 100%;
+		height: 100%;
+		background: blue;
+	}
+	.right {
+		width: 200px;
+		height: 100%;
+		background: green;
+	}
+}
+</style>
